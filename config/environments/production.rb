e4fe78dc05ce103153ca324466193395e6836f9c
Nick Dunlap <nwdunlap17@gmail.com>
2019-10-15 15:54:33 -0700

    /cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   config.action_cable.mount_path = '/cable'
-  config.action_cable.url = 'wss://lit-atoll-57112.herokuapp.com/'
+  config.action_cable.url = 'wss://lit-atoll-57112.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/localhost:*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
