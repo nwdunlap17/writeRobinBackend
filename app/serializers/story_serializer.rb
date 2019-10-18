31a4a290d1cf35719f0f7a5bc080564127da10df
Nick Dunlap <nwdunlap17@gmail.com>
2019-10-18 12:07:50 -0700

    add score to submissions

--- app/serializers/story_serializer.rb
+++ app/serializers/story_serializer.rb
@@ -15,7 +15,7 @@ class StorySerializer < ActiveModel::Serializer
         
         hash = sub.attributes
         hash[:author] = sub.user.username
-        
+        hahs[:score] = sub.tally_votes
         if (!!user_id && user_id> 0)
           #0 if user hasn't voted on this sub, 1 for positive vote, -1 for negative vote
           hash[:vote] = sub.find_user_vote(user_id)
--- app/serializers/submission_serializer.rb
+++ app/serializers/submission_serializer.rb
@@ -1,10 +1,10 @@
 class SubmissionSerializer < ActiveModel::Serializer
-  attributes :id, :content, :user_id, :canon, :position, :score
+  #This is not actually being used right now, checkout the submissions function in story_serializer
+  attributes :id, :content, :user_id, :canon, :position, :score, :author
 
-  # def author
-  #     return 'Test'
-  #     #return User.find(object.user_id).username
-  # end
+  def author
+      return User.find(object.user_id).username
+  end
 
   def score
     return object.tally_votes()
