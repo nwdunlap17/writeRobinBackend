374dd0d33ce9ff7ff8508b5c772569cff99a0c7f
Nick Dunlap <nwdunlap17@gmail.com>
2019-10-18 11:56:02 -0700

    rebreak things

--- app/serializers/submission_serializer.rb
+++ app/serializers/submission_serializer.rb
@@ -1,10 +1,10 @@
 class SubmissionSerializer < ActiveModel::Serializer
-  attributes :id, :content, :user_id, :canon, :position, :author, :score
+  attributes :id, :content, :user_id, :canon, :position, :score
 
-  def author
-      return 'Test'
-      #return User.find(object.user_id).username
-  end
+  # def author
+  #     return 'Test'
+  #     #return User.find(object.user_id).username
+  # end
 
   def score
     return object.tally_votes()
