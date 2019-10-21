b73c3f761ed4110066866e7dbcc49cc8fed64899
Nick Dunlap <nwdunlap17@gmail.com>
2019-10-21 13:11:47 -0700

    genres

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(username: 'Alice', password: 'test')
-User.create(username: 'Bob', password: 'test')
-User.create(username: 'Claire', password: 'test')
-User.create(username: 'David', password: 'test')
-User.create(username: 'Eve', password: 'test')
 
-s1 = Story.create(title: 'Once upon a time...', user_id: 1)
-s2 = Story.create(title: 'A noisy day', user_id: 2)
+# User.create(username: 'Alice', password: 'test')
+# User.create(username: 'Bob', password: 'test')
+# User.create(username: 'Claire', password: 'test')
+# User.create(username: 'David', password: 'test')
+# User.create(username: 'Eve', password: 'test')
 
-Submission.create(content: 'Ipsum Lorem Doltem', user_id:3, story_id: 1, position: 1, canon: true)
-Submission.create(content: 'Montimo Opero Fulcrum', user_id:4, story_id: 2, position: 1, canon: true)
-Submission.create(content: 'Dismis Acarum Sulcrem', user_id:5, story_id: 2, position: 2, canon: true)
+# s1 = Story.create(title: 'Once upon a time...', user_id: 1)
+# s2 = Story.create(title: 'A noisy day', user_id: 2)
+
+# Submission.create(content: 'Ipsum Lorem Doltem', user_id:3, story_id: 1, position: 1, canon: true)
+# Submission.create(content: 'Montimo Opero Fulcrum', user_id:4, story_id: 2, position: 1, canon: true)
+# Submission.create(content: 'Dismis Acarum Sulcrem', user_id:5, story_id: 2, position: 2, canon: true)
+
+Genre.create(name: 'Fantasy')
+Genre.create(name: 'Sci-Fi')
+Genre.create(name: 'Comedy')
+Genre.create(name: 'Horror')
+Genre.create(name: 'Drama')
