d80a6efc0e08c96f5719d54609bf25c5db231d75
Nick Dunlap <nwdunlap17@gmail.com>
2019-10-21 12:04:12 -0700

    LOG IN CHECK

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -4,14 +4,20 @@ class AuthenticationController < ApplicationController
 
   # POST /auth/login
   def login
+    puts 'LOGGING IN'
     userList = User.where('lower(username) = ?', params[:username].downcase)
     @user
+
+
 
     if userList.length > 0
       @user = userList[0]
     end
+    puts 'AUTHENTICATING'
 
     if @user&.authenticate(params[:password])
+
+      puts "AUTHENTICATED USER #{@user.id}"
 
       token = JWT.encode({user_id: @user.id},'5KgjiJMXTmi0jvOzwfsp')
 
@@ -24,6 +30,7 @@ class AuthenticationController < ApplicationController
     
     else
     # byebug
+      puts 'AUTHENTICATION FAILED'
       render json: { error: 'unauthorized' }, status: :unauthorized
     end
   end
