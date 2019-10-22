4c6784d5f6b0d40008eb04362e33cc4642dfd867
Nick Dunlap <nwdunlap17@gmail.com>
2019-10-22 14:21:57 -0700

    string wont unstring

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,6 +33,7 @@ class User < ApplicationRecord
     end
 
     def is_friends_with(userID)
+        userID = userID.to_i
         puts "userID class is #{userID.class}"
         puts "#{self.id} friends with #{userID}?"
         if (!!self.friends_hash[userID])
