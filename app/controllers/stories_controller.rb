1e85ea839500f849c240886524ae7902fa7e24f6
Nick Dunlap <nwdunlap17@gmail.com>
2019-10-24 13:48:37 -0700

    no double invite

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -107,10 +107,10 @@ class StoriesController < ApplicationController
                 puts "ACCESS ALLOWED, invites are"
                 params[:invites].each do |invite|
                     puts "#{invite}"
-                    if (!userlist.include?(invite.to_i)){
+                    if (!userlist.include?(invite.to_i))
                         userlist << invite.to_i
                         Invitation.create(story_id: @story.id, user_id:invite.to_i)
-                    }
+                    end
                 end
             else
                  puts "ACCESS DENIED #{user} not in #{userlist}"
